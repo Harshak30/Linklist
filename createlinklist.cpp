1c1d5ace8030978c2b3b3239baa3e7b8bd14fab1
Harsha K <59755222+Harshak30@users.noreply.github.com>
2021-08-26 21:38:00 +0530

    Add files via upload

---
+++ createlinklist.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Node
+{
+    public:
+    int data ;
+    Node *next ;
+
+    Node(int x)
+    {
+        data = x ;
+        next = NULL ;
+    }
+}*first, *last, *temp ;
+
+void create(int input)  // creates the node
+{
+    Node *new_node ;
+    new_node = new Node(input) ;
+
+    // connection part
+    if(first == NULL)
+        first = new_node ;
+
+    else
+        last -> next = new_node ;
+
+    last = new_node ;
+}
+
+void display()
+{
+    temp = first ;
+    while(temp != NULL)
+    {
+        cout << temp -> data << " -> " ;
+        temp = temp -> next ;
+    }
+    cout << "NULL" ;
+}
+
+main()
+{
+    first = last = NULL ;
+
+    // 1 2 3 4 5 6 ...... -7
+    int input ;
+
+    while(true)
+    {
+        cin >> input ;
+        if(input < 0)
+            break ;
+        create(input) ;
+    }
+    display() ;
+
+    return 0 ;
+}
